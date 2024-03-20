b03bf7ce52d04aa0eef7a9994f4637a8da45ae59
Jessica Glomb <j.glomb@douglas.de>
2024-03-20 17:20:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace NumberTreeDoubleNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number for my double tree: ");
+            int userLine = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int counter = 0; counter <= userLine; counter++)
+            {
+                Console.WriteLine();
+
+
+                for (int position = 0; position < counter; position++)
+                {
+
+                    Console.Write(counter);
+
+
+                }
+            }
+        }
+    }
+}
